c8e212f3a8dc49f483247b34251a15d4c3c9ced1
Francis <meng.francis@gmail.com>
2014-08-19 21:29:55 -0500

    add upload file slidebar and input

---
+++ public/javascripts/file_input_controller.js
@@ -0,0 +1,37 @@
+var fileExtentionRange = '.zip .rar .tar .pdf .doc .docx .xls .xlsx .ppt .pptx';
+var MAX_SIZE = 30; // MB
+
+$(document).on('change', '.lezhi-file-btn :file', function() {
+    var input = $(this);
+
+    if (navigator.appVersion.indexOf("MSIE") != -1) { // IE
+        var label = input.val();
+
+        input.trigger('fileselect', [ 1, label, 0 ]);
+    } else {
+        var label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+        var numFiles = input.get(0).files ? input.get(0).files.length : 1;
+        var size = input.get(0).files[0].size;
+
+        input.trigger('fileselect', [ numFiles, label, size ]);
+    }
+});
+
+$('.lezhi-file-btn :file').on('fileselect', function(event, numFiles, label, size) {
+    $('#lezhi-file-upload').attr('name', 'lezhi-file-upload'); // allow upload.
+    console.log("asdf");
+    var postfix = label.substr(label.lastIndexOf('.'));
+    if (fileExtentionRange.indexOf(postfix.toLowerCase()) > -1) {
+        if (size > 1024 * 1024 * MAX_SIZE ) {
+            alert('max size：<strong>' + MAX_SIZE + '</strong> MB.');
+
+            $('#lezhi-file-upload').removeAttr('name'); // cancel upload file.
+        } else {
+            $('#lezhi-file-upload-name').val(label);
+        }
+    } else {
+        alert('file type：<br/> <strong>' + fileExtentionRange + '</strong>');
+
+        $('#lezhi-file-upload').removeAttr('name'); // cancel upload file.
+    }
+});
---
+++ public/javascripts/nav_controller.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+  $('#upload').click(function(){
+    $('#upload-sidebar').sidebar({overlay: true}).sidebar('toggle');
+  });
+});
